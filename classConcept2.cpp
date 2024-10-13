b97e9908ca6ef002c951d3ddfbc14be16e5d0562
Dhruv Malhotra <malhotradhruv112004@gmail.com>
2024-10-13 17:55:21 +0530

    Add files via upload

---
+++ classConcept2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class binary
+{
+    string s;
+
+public:
+    void read(void);
+    void chk_bin(void);
+    void onces(void);
+    void display(void);
+};
+
+void binary::read(void)
+{
+    cout << "Enter a binary number" << endl;
+    cin >> s;
+}
+
+void binary::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "Not a binary number" << endl;
+            exit(0);
+        }
+    }
+}
+void binary ::onces(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '1')
+        {
+            s.at(i) = '0';
+        }
+        else
+        {
+            s.at(i) = '1';
+        }
+    }
+}
+void binary ::display(void)
+{
+    cout << "Displaying your binary  number" << endl;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+}
+
+int main()
+{
+    binary b;
+    b.read();
+    b.chk_bin();
+    b.display(); // Correctly call the function
+    b.onces();
+    b.display();
+    return 0;
+}
